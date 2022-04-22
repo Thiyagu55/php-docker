3c9b98230fa4e68f882a85ae9626e6d340c9d4bc
Thiyagu55 <thiyagu.nataraj@searce.com>
2022-04-22 15:19:06 +0530

    Test pass

--- tests/TestPhpDocker.php
+++ tests/TestPhpDocker.php
@@ -23,7 +23,7 @@ final class TestPhpDocker extends TestCase
 
         array_push($stack, 'foo');
         $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(2, count($stack));
+        $this->assertSame(1, count($stack));
 
         $this->assertSame('foo', array_pop($stack));
         $this->assertSame(0, count($stack));
